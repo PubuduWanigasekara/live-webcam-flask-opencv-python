4a4fb34cefeb56a721b4da20331f0462c4b110f5
Pubudu Arosha <it18108514@my.sliit.lk>
2021-04-04 17:47:36 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, Response
 import cv2
+
 app = Flask(__name__)
 video = cv2.VideoCapture(0)
+
 @app.route('/')
 def index():
     return "Default Message"
+
 def gen(video):
     while True:
         success, image = video.read()
@@ -12,10 +15,12 @@ def gen(video):
         frame = jpeg.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 @app.route('/video_feed')
 def video_feed():
     global video
     return Response(gen(video),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+                    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2204, threaded=True)
